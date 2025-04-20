6266001f13ac62925d8e91ee0fa0dd1d51f3d0e9
Marcos Calavia <mcalaviaf@icloud.com>
2025-04-20 18:39:23 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        # # # . .
+        . # # . .
+        . . # . .
+        `)
+})
